633b106a4a136959d2336d8e77f1115c81bbf054
ragibmondal <124962194+ragibmondal@users.noreply.github.com>
2023-07-20 13:33:39 +0600

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chatgpt-rg68.onrender.com', {
+  const response = await fetch('https://ragib543.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
